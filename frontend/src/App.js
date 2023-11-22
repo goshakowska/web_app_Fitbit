8441c069935e2c5cb1a73358b2c0ac8a5db26d51
Marta Sobol <marta.sobol.stud@pw.edu.pl>
2023-11-22 23:08:46 +0100

    Create basic react app

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="font-bold text-6xl justify-center p-6">
+      Hello World :)
+    </div>
+  );
+}
+
+export default App;
