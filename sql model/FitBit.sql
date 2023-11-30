dfb3e8e8e7cfb52bb9c170a35c1d3f5ac8140a04
Maria Polakowska <01169227@pw.edu.pl>
2023-11-30 11:42:43 +0100

    add model sql

---
+++ sql model/FitBit.sql
@@ -0,0 +1,23 @@
+INSERT INTO training_goal (training_goal_id, name) VALUES (1, 'Redukcja wagi');
+INSERT INTO training_goal (training_goal_id, name) VALUES (2, 'Zbudowanie masy');
+INSERT INTO training_goal (training_goal_id, name) VALUES (3, 'Zwiêkszenie wytrzyma³oœci');
+INSERT INTO training_goal (training_goal_id, name) VALUES (4, 'Zwiêkszenie gibkoœci');
+INSERT INTO training_goal (training_goal_id, name) VALUES (5, 'Poprawa zdrowia ogólnego');
+
+INSERT INTO equipment (equipment_id, name) VALUES (1, 'Bie¿nia');
+INSERT INTO equipment (equipment_id, name) VALUES (2, 'Rower treningowy');
+INSERT INTO equipment (equipment_id, name) VALUES (3, 'Wioœlarz');
+INSERT INTO equipment (equipment_id, name) VALUES (4, '£awka do wyciskania');    --?
+INSERT INTO equipment (equipment_id, name) VALUES (5, 'Suwnica do martwego ci¹gu'); --?
+INSERT INTO equipment (equipment_id, name) VALUES (6, 'Maszyna do przysiadów'); --?
+INSERT INTO equipment (equipment_id, name) VALUES (7, 'Dr¹¿ek do podci¹gania');
+INSERT INTO equipment (equipment_id, name) VALUES (8, 'Hantle');
+INSERT INTO equipment (equipment_id, name) VALUES (9, 'Orbitrek');
+INSERT INTO equipment (equipment_id, name) VALUES (10, 'Pi³ka do stabilizacji');
+INSERT INTO equipment (equipment_id, name) VALUES (11, 'Stepper');
+INSERT INTO equipment (equipment_id, name) VALUES (12, 'Hula-hop');
+INSERT INTO equipment (equipment_id, name) VALUES (13, 'Kostka do jogi');
+INSERT INTO equipment (equipment_id, name) VALUES (14, 'Roller');
+INSERT INTO equipment (equipment_id, name) VALUES (15, 'Suwnica na nogi');
+INSERT INTO equipment (equipment_id, name) VALUES (16, '');
+INSERT INTO equipment (equipment_id, name) VALUES (17, 'Pi³ka do stabilizacji');
