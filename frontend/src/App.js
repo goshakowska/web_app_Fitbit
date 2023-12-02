41a987ada4b7e659c6a8b4c7200fc1073fc9b8be
Marta Sobol <marta.sobol.stud@pw.edu.pl>
2023-12-02 14:43:46 +0100

    connect to django

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [text, setText] = useState([]);
+
+  useEffect(() => {
+    fetch(' http://localhost:8000/').then(res => res.json()).then(data => {
+        setText(data.message);
+      });
+    }, []);
+
+
   return (
     <div className="font-bold text-6xl justify-center p-6">
-      Hello World :)
+      Hello World :) Status django {text}
     </div>
   );
 }
