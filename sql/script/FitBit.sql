260d9cf0cffe98c30d05266c2682f1c6b9e829b1
Maria Polakowska <01169227@pw.edu.pl>
2023-12-09 20:40:30 +0100

    add gym inserts

--- sql/script/FitBit.sql
+++ sql/script/FitBit.sql
@@ -1,10 +1,11 @@
+-- training goal
 INSERT INTO training_goal (training_goal_id, name) VALUES (1, 'Redukcja wagi');
 INSERT INTO training_goal (training_goal_id, name) VALUES (2, 'Zbudowanie masy');
 INSERT INTO training_goal (training_goal_id, name) VALUES (3, 'Zwiêkszenie wytrzyma³oœci');
 INSERT INTO training_goal (training_goal_id, name) VALUES (4, 'Zwiêkszenie gibkoœci');
 INSERT INTO training_goal (training_goal_id, name) VALUES (5, 'Poprawa zdrowia ogólnego');
 
-
+-- equipment
 INSERT INTO equipment (equipment_id, name) VALUES (1, 'Bie¿nia');
 INSERT INTO equipment (equipment_id, name) VALUES (2, 'Rower treningowy');
 INSERT INTO equipment (equipment_id, name) VALUES (3, 'Wioœlarz');
@@ -20,8 +21,50 @@ INSERT INTO equipment (equipment_id, name) VALUES (12, 'Hula-hop');
 INSERT INTO equipment (equipment_id, name) VALUES (13, 'Kostka do jogi');
 INSERT INTO equipment (equipment_id, name) VALUES (14, 'Roller');
 INSERT INTO equipment (equipment_id, name) VALUES (15, 'Suwnica na nogi');
-INSERT INTO equipment (equipment_id, name) VALUES (16, '');
+INSERT INTO equipment (equipment_id, name) VALUES (16, 'Atlas');
 INSERT INTO equipment (equipment_id, name) VALUES (17, 'Pi³ka do stabilizacji');
+
+-- gym
+INSERT INTO gym (gym_id, name, phone_number, city, street, house_number, county, zip_code, 
+                 monday_opening, monday_closing, tuesday_opening, tuesday_closing, 
+                 wednesday_opening, wednesday_closing, thursday_opening, thursday_closing, 
+                 friday_opening, friday_closing, saturday_opening, saturday_closing, 
+                 sunday_opening, sunday_closing)
+VALUES 
+(1, 'FitBit Zero Fat', '123456789', 'T³uszcz', 'ul. Warszawska', '37', 'Polska', '05-240', 
+ '08:00', '21:00', '08:00', '21:00', '08:00', '20:00', '08:00', '22:00', 
+ '08:00', '20:00', '06:00', '23:00', '10:00', '18:00');
+ 
+INSERT INTO gym (gym_id, name, phone_number, city, street, house_number, county, zip_code, 
+                 monday_opening, monday_closing, tuesday_opening, tuesday_closing, 
+                 wednesday_opening, wednesday_closing, thursday_opening, thursday_closing, 
+                 friday_opening, friday_closing, saturday_opening, saturday_closing, 
+                 sunday_opening, sunday_closing)
+VALUES 
+(2, 'FitBit Plus', '111111111', 'Warszawa', 'ul. Jadowska', '100A', 'Polska', '03-761', 
+ '06:00', '22:00', '06:00', '22:00', '06:00', '22:00', '06:00', '22:00', 
+ '06:00', '22:00', '06:00', '22:00', '06:00', '22:00');
+ 
+INSERT INTO gym (gym_id, name, phone_number, city, street, house_number, county, zip_code, 
+                 monday_opening, monday_closing, tuesday_opening, tuesday_closing, 
+                 wednesday_opening, wednesday_closing, thursday_opening, thursday_closing, 
+                 friday_opening, friday_closing, saturday_opening, saturday_closing, 
+                 sunday_opening, sunday_closing)
+VALUES 
+(3, 'FitBit Minus', '111222333', 'Wo³omin', 'ul. Wileñska', '25', 'Polska', '05-200', 
+ '07:00', '20:00', '07:00', '20:00', '07:00', '22:00', '07:00', '19:00', 
+ '07:00', '20:00', '07:00', '20:00', '07:00', '20:00');
+ 
+INSERT INTO gym (gym_id, name, phone_number, city, street, house_number, county, zip_code, 
+                 monday_opening, monday_closing, tuesday_opening, tuesday_closing, 
+                 wednesday_opening, wednesday_closing, thursday_opening, thursday_closing, 
+                 friday_opening, friday_closing, saturday_opening, saturday_closing, 
+                 sunday_opening, sunday_closing)
+VALUES 
+(4, 'FitBit Malinka', '789789789', 'Zakopane', 'ul. Krzeptówki', '99', 'Polska', '34-500', 
+ '08:00', '22:00', '08:00', '22:00', '08:00', '22:00', '08:00', '22:00', 
+ '08:00', '22:00', '08:00', '22:00', '10:00', '22:00');
+ 
 
 INSERT INTO gym (gym_id, name, phone_number, city, street, house_number, county, zip_code, 
                  monday_opening, monday_closing, tuesday_opening, tuesday_closing, 
@@ -29,6 +72,25 @@ INSERT INTO gym (gym_id, name, phone_number, city, street, house_number, county,
                  friday_opening, friday_closing, saturday_opening, saturday_closing, 
                  sunday_opening, sunday_closing)
 VALUES 
-(1, 'Fitness Plus', '123-456-7890', 'City1', 'Street1', '12A', 'County1', '12345', 
- '08:00 AM', '09:00 PM', '08:00 AM', '09:00 PM', '08:00 AM', '09:00 PM', '08:00 AM', '09:00 PM', 
- '08:00 AM', '09:00 PM', '10:00 AM', '06:00 PM', '12:00 PM', '05:00 PM');
+(5, 'FitBit Polna', '159159159', 'Warszawa', 'ul. Polna', '99', 'Polska', '00-666', 
+ '06:00', '20:00', '06:00', '21:00', '07:00', '22:00', '07:00', '22:00', 
+ '06:00', '21:00', '06:00', '20:00', '16:00', '20:00');
+ 
+ INSERT INTO gym (gym_id, name, phone_number, city, street, house_number, county, zip_code, 
+                 monday_opening, monday_closing, tuesday_opening, tuesday_closing, 
+                 wednesday_opening, wednesday_closing, thursday_opening, thursday_closing, 
+                 friday_opening, friday_closing, saturday_opening, saturday_closing, 
+                 sunday_opening, sunday_closing)
+VALUES 
+(6, 'FitBit Neptun', '753753753', 'Gdañsk', 'ul. Gotycka', '20', 'Polska', '80-253', 
+ '07:00', '22:00', '07:00', '22:00', '06:00', '21:00', '07:00', '22:00', 
+ '07:00', '21:00', '08:00', '20:00', '10:00', '20:00');
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
