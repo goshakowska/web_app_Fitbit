ab2b282f3434bc5986c404010677e9e24fb921ef
Maria Polakowska <01169227@pw.edu.pl>
2023-12-09 17:20:09 +0100

    add gym insert

--- sql/script/FitBit.sql
+++ sql/script/FitBit.sql
@@ -4,6 +4,7 @@ INSERT INTO training_goal (training_goal_id, name) VALUES (3, 'Zwiêkszenie wytrz
 INSERT INTO training_goal (training_goal_id, name) VALUES (4, 'Zwiêkszenie gibkoœci');
 INSERT INTO training_goal (training_goal_id, name) VALUES (5, 'Poprawa zdrowia ogólnego');
 
+
 INSERT INTO equipment (equipment_id, name) VALUES (1, 'Bie¿nia');
 INSERT INTO equipment (equipment_id, name) VALUES (2, 'Rower treningowy');
 INSERT INTO equipment (equipment_id, name) VALUES (3, 'Wioœlarz');
@@ -21,3 +22,13 @@ INSERT INTO equipment (equipment_id, name) VALUES (14, 'Roller');
 INSERT INTO equipment (equipment_id, name) VALUES (15, 'Suwnica na nogi');
 INSERT INTO equipment (equipment_id, name) VALUES (16, '');
 INSERT INTO equipment (equipment_id, name) VALUES (17, 'Pi³ka do stabilizacji');
+
+INSERT INTO gym (gym_id, name, phone_number, city, street, house_number, county, zip_code, 
+                 monday_opening, monday_closing, tuesday_opening, tuesday_closing, 
+                 wednesday_opening, wednesday_closing, thursday_opening, thursday_closing, 
+                 friday_opening, friday_closing, saturday_opening, saturday_closing, 
+                 sunday_opening, sunday_closing)
+VALUES 
+(1, 'Fitness Plus', '123-456-7890', 'City1', 'Street1', '12A', 'County1', '12345', 
+ '08:00 AM', '09:00 PM', '08:00 AM', '09:00 PM', '08:00 AM', '09:00 PM', '08:00 AM', '09:00 PM', 
+ '08:00 AM', '09:00 PM', '10:00 AM', '06:00 PM', '12:00 PM', '05:00 PM');
