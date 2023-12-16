7f2b1ce315667148476a7500ac17ea36dd942941
mtwarow1 <01169255@pw.edu.pl>
2023-12-16 02:35:07 +0100

    added gyms table (yet to be styled)

--- frontend/src/client/components/FitnessClubs.js
+++ frontend/src/client/components/FitnessClubs.js
@@ -1,7 +1,53 @@
+import React, { useState, useEffect } from "react";
+import {Table} from 'reactstrap';
+
 function FitnessClubs()
 {
+    const [gyms, setGyms] = useState([])
+
+    const getGyms = async (event) => {
+        try {
+            const response = await fetch('http://localhost:8000/client/gyms_list/', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              }});
+
+            if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            setGyms(data.gyms)
+
+          } catch (error) {
+            console.error('Error:', error);
+          };
+    }
+
+    useEffect(() => {getGyms()}, []);
+
     return (
-        <h1> Tu będą kluby </h1>
+        <Table bordered hover responsive >
+  <thead>
+    <tr>
+      <th>
+        Nazwa
+      </th>
+      <th>
+        Adres
+      </th>
+    </tr>
+  </thead>
+  <tbody>
+  {gyms.length > 0 && gyms.map((gym, index) => (
+                    <tr key={index}>
+                        <th scope="row">{gym[1]}</th>
+                        <td> {gym[2]}, {gym[3]}, {gym[4]}</td>
+                    </tr>
+                ))}
+  </tbody>
+</Table>
     );
 
 }
