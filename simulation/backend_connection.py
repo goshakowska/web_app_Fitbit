9e10fd6433993b6ccb67490f7025a26da6eb4568
Maria Polakowska <01169227@pw.edu.pl>
2023-12-17 12:19:18 +0100

    gyms and clients

---
+++ simulation/backend_connection.py
@@ -0,0 +1,46 @@
+import requests
+
+
+# CONNECTION
+def connection():
+    api_url = "http://localhost:8000/simulation/test_connection/"
+    headers = {"Content-Type": "application/json"}
+
+    body = {"message": "We are connected"}
+
+    try:
+        response = requests.post(api_url, json=body, headers=headers)
+
+        if response.status_code == 200:
+            print("Success")
+        else:
+            print(f"Error response {response.status_code}  {response.text}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {str(e)}")
+
+
+def get_clients():
+    api_url = "http://localhost:8000/simulation/all_clients/"
+    headers = {"Content-Type": "application/json"}
+    try:
+        response = requests.post(api_url, headers=headers)
+        if response.status_code == 200:
+            return response.json().get('clients', [])
+        else:
+            print(f"Error response {response.status_code}  {response.text}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {str(e)}")
+
+
+def get_gyms():
+    api_url = "http://localhost:8000/simulation/all_gyms/"
+    headers = {"Content-Type": "application/json"}
+    try:
+        response = requests.post(api_url, headers=headers)
+        if response.status_code == 200:
+            return response.json().get('gyms', [])
+        else:
+            print(f"Error response {response.status_code}  {response.text}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {str(e)}")
--- simulation/main.py
+++ simulation/main.py
@@ -1,30 +1,45 @@
-from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtWidgets import QApplication, QMainWindow, QListWidgetItem
 from ui_simulation_window import Ui_MainWindow
 import sys
-import requests
+import backend_connection as bc
 
 class MainScreen(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self._send_to_database)
+        self._clients()
+        self._gyms()
 
-        # CONNECTION
-        api_url = "http://localhost:8000/simulation/test_connection/"
-        headers = {"Content-Type": "application/json"}
+    def _send_to_database(self):
+        pass
 
-        body = {"message": "We are connected"}
+    def _clients(self):
+        self.ui.clientList.clear()
+        clients = bc.get_clients()
+        clients = sorted(clients, key=lambda client: client[0])
+        for client in clients:
+            description = f'{client[0]}.\t {client[1]}\t {client[2]}'
+            item = QListWidgetItem(description)
+            item.id = client[0]
+            self.ui.clientList.addItem(item)
 
-        try:
-            response = requests.post(api_url, json=body, headers=headers)
+    def _gyms(self):
+        self.ui.gymList.clear()
+        gyms = bc.get_gyms()
+        gyms = sorted(gyms, key=lambda gym: gym[0])
+        for gym in gyms:
+            description = f'{gym[0]}.\t{gym[1]}'
+            item = QListWidgetItem(description)
+            item.id = gym[0]
+            self.ui.gymList.addItem(item)
 
-            if response.status_code == 200:
-                print("Success")
-            else:
-                print(f"Error response {response.status_code}  {response.text}")
+    def _exercises(self):
+        pass
 
-        except requests.exceptions.RequestException as e:
-            print(f"Error: {str(e)}")
+
+
 
 
 def guiMain(args):
