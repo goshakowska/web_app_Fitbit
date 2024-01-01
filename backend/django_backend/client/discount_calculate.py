ad53d0fac8a32f2bc084c5f8855591e5cb8de8c7
Maria Polakowska <01169227@pw.edu.pl>
2024-01-01 21:07:08 +0100

    correct date to string

--- backend/django_backend/client/discount_calculate.py
+++ backend/django_backend/client/discount_calculate.py
@@ -1,10 +1,10 @@
-import datetime
+from datetime import datetime, date
 
 def calcucate_price_after_discount(price_before, discount):
     return price_before * (100 - discount) / 100
 
-def str_date(date):
-    return datetime.strptime(date, "%Y-%m-%d") if date else None
+def str_date(date_obj):
+    return date_obj.strftime("%Y-%m-%d") if date_obj else None
 
-def str_hour(date):
-    return datetime.strptime(date, "%H:%M:%S") if date else None
+def str_hour(time_obj):
+    return time_obj.strftime("%H:%M:%S") if time_obj else None
