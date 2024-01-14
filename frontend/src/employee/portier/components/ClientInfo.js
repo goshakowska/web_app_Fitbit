8f442000112d0e4673dac9447395f836f3c3ede2
mkozlo12 <01169195@pw.edu.pl>
2024-01-14 20:43:45 +0100

    modified client info component for portier

--- frontend/src/employee/portier/components/ClientInfo.js
+++ frontend/src/employee/portier/components/ClientInfo.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import '../styles/ClientInfo.css';
 import { Grid } from '@mui/material';
 import { useLocation } from 'react-router-dom';
 import StatusToggle from './StatusToggle';
 import RegisterTrainingPopup from './RegisterTrainingPopup';
+import { Container } from 'react-bootstrap';
 
 function ClientInfo(props) {
   const location = useLocation();
@@ -15,6 +16,7 @@ function ClientInfo(props) {
 
  return (
   <div>
+    <Container className='portier-client-info'>
     <Grid className="profile">
         <img src={"../user.png"} alt="profile" />
         <h2 className="name-title-label">  {client.name} {client.surname}</h2>
@@ -25,24 +27,25 @@ function ClientInfo(props) {
           alignItems="center" >
       <Grid className="client-info" spacing={5} item xs={6}>
         <h3 className="title-label">Dane klienta</h3>
-        <p className="label">Email: {client.email}</p>
-        <p className="label">Numer telefonu: {client.phoneNumber}</p>
-        <button>Show client's timetable</button>
+        <p className="labels">Email: {client.email}</p>
+        <p className="labels">Numer telefonu: {client.phoneNumber}</p>
+        <button className='button-client-info'>Show client's timetable</button>
       </Grid>
       <Grid className="client-card" spacing={5} item xs={6}>
         <h3 className="title-label">Karnet</h3>
-        <p className="label">Typ: {client.cardType}</p>
-        <p className="label"><StatusToggle initialStatus={client.cardStatus}/></p>
+        <p className="labels">Typ: {client.cardType}</p>
+        <p className="labels"><StatusToggle initialStatus={client.cardStatus}/></p>
       </Grid>
       <Grid className="training-info" spacing={5} item xs={6}>
-          <p className="label">Najbliższy trening: {client.nextTraining}</p>
+          <p className="labels">Najbliższy trening: {client.nextTraining}</p>
           <RegisterTrainingPopup/>
       </Grid>
       <Grid className="card-dates" spacing={5} item xs={6}>
-        <p className="label">Data aktywacji: {client.activationDate}</p>
-        <p className="label">Data ważności: {client.expirationDate}</p>
+        <p className="labels">Data aktywacji: {client.activationDate}</p>
+        <p className="labels">Data ważności: {client.expirationDate}</p>
       </Grid>
     </Grid>
+    </Container>
 </div>
  );
 };
