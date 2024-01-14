4855d366baa15e47beb7d2c7eea707d0ee1e76e6
mkozlo12 <01169195@pw.edu.pl>
2024-01-14 20:33:17 +0100

    added group classes info component (for the trainer functionality)

---
+++ frontend/src/employee/trainer/components/GroupClassesInfo.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react';
+import '../styles/groupClassesInfo.css';
+// import { Grid } from '@mui/material';
+import { useLocation } from 'react-router-dom';
+// import StatusToggle from './StatusToggle';
+// import RegisterTrainingPopup from './RegisterTrainingPopup';
+import { Carousel, Col, Row } from "react-bootstrap";
+import employeeToken from "../../EmployeeToken";
+import Header from '../../../client/components/Header';
+import '../styles/groupClassesInfo.css';
+
+function GroupClassInfo(props) {
+  const location = useLocation();
+  console.log(props, " props");
+  console.log(location, " useLocation Hook");
+
+
+  const class_id = location.state?.chosenTraining;
+
+  const {userName} = employeeToken();
+
+  const [groupClassName, setgroupClassName] = useState("");
+  const [groupClassDescription, setgroupClassDescription] = useState("");
+
+
+  const getGroupClassDescription = async (event) => {
+
+    try {
+        const response = await fetch('http://localhost:8000/trainer/describe_group_class/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },body: JSON.stringify({ class_id: class_id})});
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+        console.log(data.name);
+        console.log(data.description);
+        setgroupClassName(data.name);
+        setgroupClassDescription(data.description);
+
+      } catch (error) {
+        console.error('Error:', error);
+      };
+}
+
+useEffect(() => {getGroupClassDescription()}, []);
+
+ return (
+  <div>
+    <Row>
+        <h1 className="text" >Witaj {userName()}</h1>
+    </Row>
+    <Row>
+    <Col>
+    <Carousel className="custom-carousel" style={{ width: "50%", margin: "auto" }}>
+      <Carousel.Item>
+      <img className="d-block mx-auto" width={900} height={600} src={"../grupowy_trening.jpg"} alt={groupClassName} style={{ margin: 'auto' }} />
+        <Carousel.Caption>
+          <h3 className="text">{groupClassName}</h3>
+          <p className="custom-caption">HEALTH. STRENGTH. POWER.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img className="d-block mx-auto" width={900} height={600} src={"../grupowy_trening_1.jpg"} alt={groupClassName} style={{ margin: 'auto' }}/>
+        <Carousel.Caption>
+          <h3 className="text">{groupClassName}</h3>
+          <p className="custom-caption">HEALTH. STRENGTH. POWER.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img className="d-block mx-auto" width={900} height={600} src={"../grupowy_trening_2.jpg"} alt={groupClassName} style={{ margin: 'auto' }}/>
+        <Carousel.Caption>
+          <h3 className="text">{groupClassName}</h3>
+          <p className="custom-caption">HEALTH. STRENGTH. POWER.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </Col>
+    <Col>
+    <h2 className="text">Zajęcia grupowe - {groupClassName}</h2>
+    <p>{groupClassDescription}</p>
+    <p className="text"></p>
+    </Col>
+    </Row>
+</div>
+ );
+};
+
+
+
+export default GroupClassInfo;
