fe80b6791468544e04f3c3533362867557387912
mkozlo12 <01169195@pw.edu.pl>
2024-01-14 20:44:16 +0100

    added modification for side bar component to portier

---
+++ frontend/src/employee/portier/components/SideBar.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+import "../styles/styles.css"
+import employeeToken from '../../EmployeeToken';
+
+
+function SideBarPortier() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const {userName, logout} = employeeToken();
+
+  const handleLogout = () => {
+    logout();
+  }
+
+  return (
+    <div >
+      <Navbar>
+        <NavbarBrand href="/"></NavbarBrand>
+        <NavbarToggler className= 'user-circle-text-style' onClick={toggle} > <span>{userName()}</span> </NavbarToggler>
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className='sidebar-style dark-green' navbar>
+            <NavItem className='sidebar-pos-style'>
+              <NavLink className='text-style-sidebar' href="/">
+                Wyświetl bazę danych klientów
+                </NavLink>
+            </NavItem>
+            <NavItem className='sidebar-pos-style' onClick={handleLogout}>
+            <NavLink className='text-style-sidebar' href="/" >
+                Wyloguj się
+              </NavLink>
+              </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default SideBarPortier;
