feef7add4701bcdf6e01f1bcfa6fda0ab64be68c
Marta Sobol <marta.sobol.stud@pw.edu.pl>
2024-01-19 21:27:39 +0100

    correct

--- backend/django_backend/django_backend/urls.py
+++ backend/django_backend/django_backend/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from views import test_message, modify_number, employee_login
+from views import employee_login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
